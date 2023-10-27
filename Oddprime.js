fb806f90be544a40cd4e1895bdbb497cf6831d16
yasaswinibera <149067084+yasaswinibera@users.noreply.github.com>
2023-10-27 17:18:14 +0530

    Oddprime.js

--- Oddprime.js
+++ Oddprime.js
@@ -1,52 +1,72 @@
 
-// This is my implementation for printing One-digit-difference prime numbers
+// This is my implementation for printing One-digit-difference prime numbers.
+/*The one-digit-difference prime numbers are those which have the difference 
+between consective digits of the number as one and also the number has to be prime.*/
 
-/**This is a function to check whether the number is prime or not*/
+/**This is a function to check whether the number is prime or not
+ * @param number takes input number to check if it is prime or not
+ * @return true if it is prime or false if it is not prime
+*/
  
-function isPrime(num) {
-    var sqrtnum=Math.floor(Math.sqrt(num));
-      var prime = num != 1;
-      for(var i=2; i<sqrtnum+1; i++) { 
-          if(num % i == 0) {
-              prime = false;
-              break;
-          }
-      }
-      return prime;  //return true if it is prime
-  }
-  // This a function to verify whether the number has one-digit-difference
-  function check(number)
-  {
-    let sum = 0
-    let flag
+function isPrime(number) 
+{
+    let sqrtnum=Math.floor(Math.sqrt(number))
+    let prime = number != 1
+    for(let i=2; i<sqrtnum+1; i++) 
+    { 
+        if(number % i == 0) 
+        {
+            prime = false
+            break
+        }
+    }
+    return prime  //return true if it is prime
+}
+
+/**This a function to verify whether the consecutive digits in the input number
+ * has the difference as one between them.
+ *@param number takes the number as function parameter.
+ *@return the boolean value*/
+
+function oddCheck(number)
+{
+    let difference = 0
+    let result 
     const digits = String(number).split('').map(Number)
-  for(let i=1;i<(digits.length);i++)
-  {
-    sum = digits[i-1]-digits[i]
-    if (sum === 1 || sum === -1){
-        flag = 0
-    }
-    else{
-        flag = 1
-    }
-  }
-  return flag
-  }
- /*This is a function create an array with numbers that are satisfying
+    for(let i=1;i<(digits.length);i++)
+        {
+            difference = digits[i-1]-digits[i]
+            if (difference === 1 || difference === -1)
+            {
+              result = 0  
+            }
+            else
+            {
+               result = 1
+            }
+        }
+    return result
+}
+ /**This is a function to create an array with numbers that are satisfying
   one-digit-difference and prime number*/
-  function OneDigitDifference()
+function oddPrimeNumber()
   {
-     let result = []
-    
-     for (let i=0 ;i <= 10000000;i++){
-       
-         if (check(i) === 0 && isPrime(i) ===true)
-         {
-            result.push(i)    //adding the number into the array
-         }
-     }
-     return result
+    //creating an empty array to store numbers.
+    let result = []
+    let count=0
+    for (let i=0 ;i <= 10000000;i++)
+    {
+        if (oddCheck(i) === 0 && isPrime(i) ===true)
+        {
+            result.push(i)    //adding the number into the array.
+            count++
+        }
+    }
+    console.log(`The count of the numbers are ${count}`)
+    console.log("The list of the numbers that are ODD-Prime is")
+    return result    // returns the resultant array of numbers.
   }
-  // printing the numbers that are prime and having one-digit-difference
-  console.log("The list of numbers that have one-digit-difference and prime are")
-  console.log(OneDigitDifference())
+  // printing the numbers that are prime and having one-digit-difference.
+  console.log(oddPrimeNumber())
+   
+
